71b218a2e3fe2b37132b2da681c59b73f56a70a1
Musthafa Faishal <musthafafaishal@gmail.com>
2023-06-20 16:40:59 +0700

    refactor: code refactor

--- app/src/main/java/com/dicoding/storyapp/detail/DetailActivity.kt
+++ app/src/main/java/com/dicoding/storyapp/detail/DetailActivity.kt
@@ -107,13 +107,13 @@ class DetailActivity : AppCompatActivity() {
                         viewModel.deleteStory(story)
                     fabDetailSaveBookmark.setImageDrawable(ContextCompat.getDrawable(
                         this@DetailActivity,
-                        R.drawable.baseline_favorite_48
+                        R.drawable.baseline_favorite_border_48
                     ))
                 } else {
                     viewModel.saveStory(story)
                     fabDetailSaveBookmark.setImageDrawable(ContextCompat.getDrawable(
                         this@DetailActivity,
-                        R.drawable.baseline_favorite_border_48
+                        R.drawable.baseline_favorite_48
                     ))
                 }
             }
